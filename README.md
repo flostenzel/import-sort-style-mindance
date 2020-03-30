770cde80ca5986f75f1e69094de8997725d48bf7
Florian Stenzel <florian@mindance.de>
2020-03-30 10:35:55 +0200

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
 # import-sort-style-mindance
+
+Sensible [import sorting](https://github.com/renke/import-sort/) style for React Native projects.
+
+## Usage
+
+### Installation
+
+```
+npm install --save-dev import-sort-cli import-sort-style-mindance
+```
+
+### Setup
+
+Add the following to your `package.json`:
+
+```json
+"importSort": {
+  ".js, .jsx": {
+    "parser": "babylon",
+    "style": "mindance"
+  }
+}
+```
+
+### Typescript
+
+You can use a different parser for Typescript files. More info [here](https://github.com/renke/import-sort#using-a-different-style-or-parser).
+
+```
+npm install --save-dev import-sort-parser-typescript
+```
+
+Add this to your `package.json`:
+
+```diff
+"importSort": {
+  ".js, .jsx": {
+    "parser": "babylon",
+    "style": "mindance"
+  },
++ ".ts, .tsx": {
++   "parser": "typescript",
++   "style": "mindance"
++ }
+}
+```
+
+## Style
+
+```javascript
+// Modules with side effects (not sorted internally because order may matter)
+import 'c';
+import 'a';
+
+// React (Native) modules
+import React from 'react';
+import { Text, ... } from 'react-native';
+
+// Node.js modules
+import { readFile } from 'fs';
+
+// Installed modules
+import axios from 'axios';
+
+// "External" project modules ("../")
+// "Internal" project modules ("./")
+import Divider from '../components/Divider';
+import Bubble from './Bubble';
+
+// Custom styles
+// Custom constants
+// Custom types
+// Resource files (.png, .jpg, .jpeg, .svg)
+import shadow from '../styles/base' ;
+import MD_GREEN from '../constants/base' ;
+import type SESSION from '../types';
+import image from '**/foo.png'
+
+```
+
+### References
+
+- https://github.com/renke/import-sort/tree/master/packages/import-sort-style-module
+- https://github.com/pietile/import-sort-style-pietile
+- https://github.com/nictar/import-sort-order-rn
---
+++ src/index.js
@@ -0,0 +1,130 @@
+function isResourceModule(imported) {
+  const { moduleName } = imported;
+  return (
+    moduleName.endsWith('.png') ||
+    moduleName.endsWith('.svg') ||
+    moduleName.endsWith('.jpg') ||
+    moduleName.endsWith('.jpeg')
+  );
+}
+
+function isCustomTypeModule(imported) {
+  return imported.moduleName.endsWith('/types')
+}
+
+function isConstantModule(imported) {
+  return imported.moduleName.endsWith('/constants/base')
+}
+
+function isStyleModule(imported) {
+  return imported.moduleName.includes('/styles/')
+}
+
+function isReactModule(imported) {
+  return imported.moduleName === 'react';
+}
+
+function isReactNativeModule(imported) {
+  return (
+    imported.moduleName === 'react-native' ||
+    imported.moduleName.startsWith('react-native/')
+  );
+}
+
+function isInternalModule(imported) {
+  return imported.moduleName.startsWith('./');
+}
+
+function isExternalModule(imported) {
+  return imported.moduleName.startsWith('../');
+}
+
+function style(api, file) {
+  const {
+    alias,
+    and,
+    not,
+    dotSegmentCount,
+    hasNoMember,
+    isAbsoluteModule,
+    isInstalledModule,
+    isNodeModule,
+    isRelativeModule,
+    moduleName,
+    naturally,
+    unicode,
+  } = api;
+  return [
+    // import 'foo'
+    { match: and(hasNoMember, isAbsoluteModule) },
+    { separator: true },
+
+    // import './foo'
+    { match: and(hasNoMember, isRelativeModule) },
+    { separator: true },
+
+    // import React from 'react'
+    // import { View, ... } from 'react-native'
+    // import type { ViewStyleProp, ... } from 'react-native/**'
+    { match: isReactModule, sortNamedMembers: alias(unicode) },
+    { match: isReactNativeModule, sortNamedMembers: alias(unicode) },
+    { separator: true },
+
+    // import ... from 'fs' (Node modules)
+    {
+      match: isNodeModule,
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import ... from 'foo'
+    {
+      match: isInstalledModule(file),
+      sort: moduleName(unicode),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import ... from '../projectFoo' (non-resource)
+    // import ... from './projectFoo' (non-resource)
+    {
+      match: and(isExternalModule, not(isResourceModule), not(isCustomTypeModule), not(isConstantModule)),
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    {
+      match: and(isInternalModule, not(isResourceModule), not(isCustomTypeModule), not(isConstantModule)),
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+
+    // import shadow from '../styles/base' ;
+    // import MD_GREEN from '../constants/base' ;
+    // import type SESSION from '../types';
+    // import image from '**/foo.png'
+    { separator: true },
+    {
+      match: and(isStyleModule, not(isResourceModule), not(isCustomTypeModule), not(isConstantModule)),
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    {
+      match: and(isConstantModule, not(isResourceModule), not(isCustomTypeModule)),
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    {
+      match: and(isCustomTypeModule, not(isResourceModule)),
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    {
+      match: isResourceModule,
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+  ];
+}
+
+module.exports = style;
